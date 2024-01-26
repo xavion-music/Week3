8f471456c2f9247ad28ad9a1e746eca0b027c2e9
palak <palakchaudhari311@gmail.com>
2024-01-26 13:03:29 -0500

    Added "For Pushing"

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -16,6 +16,7 @@ public class Week3 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("This is week 3 class");
+        System.out.println("For Pushing");
     }
     //trying fetch and merge
 }
